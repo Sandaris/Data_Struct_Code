bc270c3520d3f569183e36f9906f10bb371db28e
Not Irfan <149769702+irfanrzl@users.noreply.github.com>
2025-04-28 12:23:16 +0800

    getdata function and struct container

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,6 @@ using fs::path;
 using namespace std;
 
 //test
-
 int main() 
 {
     string fileContent = readCSVFile("cleaned_reviews.csv");
@@ -24,6 +23,18 @@ int main()
     {
         cout << line << endl;
     }
+    
+    string cleaned_review = "filepath";
+    dataContainer2D cleaned_review_data = getData(cleaned_review);
+
+    cout << cleaned_review_data.error << endl;
+    cout << "Fields: ";    
+    cout << cleaned_review_data.fields[1] << endl;
+    cout << cleaned_review_data.data[0][0] << endl;
+    cout << cleaned_review_data.x << endl;
+    cout << cleaned_review_data.y << endl;
+
+    freeContainer(cleaned_review_data);
 
     return 0;
 }
